77d3244419fe6d2f706a37c708d3a7adbdf84dcd
PanosBt <panbout@hotmail.gr>
2024-10-23 20:03:02 +0000

    add project page texts

--- src/const.js
+++ src/const.js
@@ -4,43 +4,103 @@ export const PROJECTS = {
         chips: ['Branding'],
         hero_text: 'Α well-established florist in Ilion since 1974',
         desc_html: `
-        Working on the branding for Lazaros Flowers, I established a fresh and elegant identity. Launching with a redesigned logo, I chose a color close to Metallic Seaweed for vibrancy.
-        <br><br>
-        Following that, I crafted business cards and stickers to enhance their overall branding.
-        `,
+            Working on the branding for Lazaros Flowers, I established a fresh and elegant identity. Launching with a redesigned logo, I chose a color close to Metallic Seaweed for vibrancy.
+            <br><br>
+            Following that, I crafted business cards and stickers to enhance their overall branding.
+            `
+        ,
         main_heading: 'Stickers & Business Card'
     },
     'thomas_vision': {
         title: 'Thomas Vision',
         chips: ['Branding', 'MockUps'],
+        hero_text: 'A luxury yachting company offering VIP transfers and cruises',
+        desc_html: `
+            Creating promotional materials for Thomas’ Vision Yachting,
+            I designed a brochure and signs to showcase their luxury services.
+            The brochure captures the essence of their motto: 'Escape, Explore, Enjoy!' with vibrant imagery and compelling information.
+            <br><br>
+            The signs, in two sizes, enhance visibility and invite tourists to embark on unforgettable cruises.
+            `
+        ,
+        main_heading: 'Brochure, Sign 160X070, 100Χ060'
     },
     'boat_up': {
         title: 'Boat Up',
         chips: ['Branding'],
+        hero_text: 'A web app for managing boat reservations',
+        desc_html: `
+            I created a brand for Boat Up, a web app designed for managing boat reservations.
+            <br><br>
+            The branding utilizes a vibrant color palette, featuring Blue (#1B80FA), Light Blue (#87BCFA), and Lightest Blue (#E3EBFB), to convey a sense of trust and clarity.
+        `,
+        main_heading: 'Brochure'
     },
     'nydris': {
         title: 'Nydris',
         chips: ['UI/UX Design'],
+        hero_text: 'An elegant website for Nydris villa with blue and white aesthetics',
+        desc_html: `
+            I developed the corporate identity and villa template for Nydris, a luxury property in Lefkada.
+            <br>
+            The design features an elegant blue and white palette, reflecting the island's serene atmosphere. The website highlights villa amenities, activities in Lefkada, floor plans, and a gallery showcasing the villa’s interior and exterior, providing all essential information for a luxurious stay.
+        `,
+        main_heading: 'Website & Responsive View'
     },
     'rent_me': {
         title: 'Rent me',
         chips: ['UI/UX Design'],
+        hero_text: 'A car rental with luxurious and modern cars in Preveza',
+        desc_html: `
+            Designing the website for Rent Me, I focused on creating a warm, inviting space.
+            <br><br>
+            Featuring orange and white tones, the site allows users to view the fleet of luxurious, modern cars and book the one they need for their vacation with ease and convenience.
+        `,
+        main_heading: 'Website & Responsive View'
     },
     'greek_brand_new': {
         title: 'Greek Brand New',
         chips: ['UI/UX Design'],
+        hero_text: 'Α dynamic exhibition that showcases Greek companies',
+        desc_html: `
+            I created the website for Greek Brand New, highlighting an exhibition through a bold color palette of electric blue and bright yellow.
+            <br><br>
+            The design incorporates elements of neo-brutalism, guiding users to explore exhibitors and learn how to attend with ease.
+        `,
+        main_heading: 'Website & Responsive View'
     },
     'butterfly_rent_a_car': {
         title: 'Butterfly rent a car',
         chips: ['UI/UX Design'],
+        hero_text: 'A reliable car and motorcycle rentals in central Rhodes since 1970',
+        desc_html: `
+            Redesigning the website for Butterfly Rent a Car, I aimed to create a modern and user-friendly platform.
+                <br><br>
+            The site features a vibrant blue and yellow color scheme with geometric elements, offering essential information about Rhodes, attractions to explore, and transportation options from the airport to the rental location, including motorcycle rentals.
+        `,
+        main_heading: 'Website & Responsive View'
     },
     'reality_ripples': {
         title: 'Reality Ripples',
         chips: ['UI/UX Design'],
+        hero_text: 'Αn escape room company based in Athens',
+        desc_html: `
+            I developed a website for Reality Ripples, an escape room company based in Athens. The site showcases their immersive themed rooms, including Deadlock, Boogeyman, Deadlock 2, and Middlearth Tales, using a dark palette of black with red accents to match the atmospheric feel.
+            <br>
+            The design focuses on enhancing the eerie and cinematic experience, bringing the rooms' stories to life for players.
+        `,
+        main_heading: 'Website & Responsive View'
     },
     'yitai': {
         title: 'Yitai',
         chips: ['UI/UX Design'],
+        hero_text: 'Α real-world data and AI for healthcare innovation.',
+        desc_html: `
+        I designed the website for Yitai, a company leveraging real-world data and AI to drive healthcare innovation.
+            <br><br>
+        The site features a futuristic design in shades of purple, aimed at highlighting the company's cutting-edge approach and mission to improve patient care through advanced analytics and machine learning.
+        `,
+        main_heading: 'Website & Responsive View'
     },
     'pet365': {
         title: 'Pet 365',
@@ -48,8 +108,7 @@ export const PROJECTS = {
         hero_text: 'A Small family company with boats in Lakki Leros for renting',
         desc_html: `
             Taking on the design for Pet365, I crafted a modern identity that reflects care and love for pets. Starting with a fresh logo, I used shades of blue and purple to bring vibrancy.
-            <br>
-            <br>
+            <br><br>
             Then, I designed the website, focusing on a clean, user-friendly, and welcoming experience, with soft pastel tones used throughout the platform.
         `,
         main_heading: 'Website & Responsive view',
