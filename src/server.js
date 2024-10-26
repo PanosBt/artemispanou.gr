50ce3fc73959a514cd38cd3f0689d9f9fea7f86a
PanosBt <panbout@hotmail.gr>
2024-10-26 23:11:32 +0300

    add cv

--- src/server.js
+++ src/server.js
@@ -3,16 +3,26 @@ import render from '@koa/ejs';
 import serve from 'koa-static';
 import url from 'url';
 import path from 'path';
+import { hashFile } from 'hasha';
 
 import router from './routes.js';
+import { ASSETS_VERSION } from './const.js'
 
-const app = new Koa();
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const app = new Koa(),
+    __dirname = url.fileURLToPath(new URL('.', import.meta.url)),
+    cvHash = await hashFile(path.resolve(__dirname, '../static/cv.pdf'))
+;
 
 render(app, {
     root: path.join(__dirname, 'views'),
     viewExt: 'ejs',
     layout: false
+});
+
+app.use(async (ctx, next) => {
+    ctx.state.assets_version = ASSETS_VERSION;
+    ctx.state.cvHash = cvHash;
+    await next();
 });
 
 app.use(router.routes()).use(router.allowedMethods());
